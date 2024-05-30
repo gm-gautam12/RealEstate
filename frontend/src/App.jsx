d8ab0b43c8617fa769c5390ec86bfb53c34bf323
Gautam Mishra <njoygautam12@gmail.com>
2024-05-31 02:28:59 +0530

    chore: Add react-router-dom to package dependencies and update App.jsx to use react-router-dom for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./components/SignIn.components";
+import SignUp from "./components/SignUp.components";
+import About from "./components/About.components";
+import Home from "./components/Home.components";
+import Profile from "./components/Profile.components";
 
 const App = () => {
-    return(
-        <div>App</div>
-    )
+    return <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+       </Routes>
+    </BrowserRouter>
+    
 }
 
 export default App;
