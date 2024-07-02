afe21b554f926b9ab34a3527baf557aa94d13d62
Gautam Mishra <njoygautam12@gmail.com>
2024-07-02 13:33:41 +0530

    chore: Update file path for serving frontend build in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -46,5 +46,5 @@ app.use("/api/listing",listingRouter);
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client','dist','index.html'));
+    res.sendFile(path.join(__dirname, 'frontend','dist','index.html'));
 });
