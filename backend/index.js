30deebf7f4033921ad1f7561c7bc484aec541a18
Gautam Mishra <njoygautam12@gmail.com>
2024-07-02 13:19:51 +0530

    chore: Add static file serving for frontend build in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import  authRouter from './routes/auth.routes.js';
 import listingRouter from './routes/listing.routes.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';    
 
 
 dotenv.config();
@@ -16,6 +17,8 @@ mongoose.connect(process.env.MONGO).then( ()=> {
     console.log(err);
 })
 
+
+const  __dirname = path.resolve();
 
 
 const app = express();
@@ -39,3 +42,9 @@ app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 
 app.use("/api/listing",listingRouter);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client','dist','index.html'));
+});
